451a6fb90ebce0f81806e273423e82ce5cd64de4
mikaelsfm <mikael.schwind-terceiro@cea.com.br>
2025-05-16 16:03:16 -0300

    feat: update version to 0.1.3 in configuration files for shopping addons

--- eitri-shopping-addons-home/eitri-app.conf.js
+++ eitri-shopping-addons-home/eitri-app.conf.js
@@ -5,7 +5,7 @@ module.exports = {
   "eitri-luminus": "2.0.4",
   "eitri-bifrost": "3.8.0",
   "eitri-commons": "2.1.2",
-  "version": "0.1.2",
+  "version": "0.1.3",
   "public-key": "68825733-1313-41ad-ac46-7defb3cc98bf",
   "applicationId": "5e5c15f7-65cc-4c57-96b0-cbd55ddcbc5b",
   "id": "97f07847-0263-4681-a419-84d64e697078",
