5479503a2db0a480695186e30fabb1ed27ee61bd
Diogo Fagundes <diogo.fagundes-terceiro@cea.com.br>
2025-05-16 16:42:31 -0300

    ci: update eitriapp version

--- eitri-shopping-addon-vtex-newtail/eitri-app.conf.js
+++ eitri-shopping-addon-vtex-newtail/eitri-app.conf.js
@@ -5,7 +5,7 @@ module.exports = {
   "eitri-luminus": "2.0.0",
   "eitri-bifrost": "3.8.0",
   "eitri-commons": "2.1.2",
-  "version": "0.1.3",
+  "version": "0.1.0",
   "public-key": "26b6edd6-a5de-4a01-8f91-00d3ff6f54d1",
   "applicationId": "5e5c15f7-65cc-4c57-96b0-cbd55ddcbc5b",
   "id": "4c2be2ef-4415-4f99-af2f-39771f84da77",
